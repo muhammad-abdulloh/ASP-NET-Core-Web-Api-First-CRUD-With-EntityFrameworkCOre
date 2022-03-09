53bb87bf1035f4a67c853771a31eb47b5fbc9804
Clever Coder <clevercoderr@gmail.com>
2022-03-09 16:30:20 +0500

    Controller update

--- ProjectDemo/Controllers/UserController.cs
+++ ProjectDemo/Controllers/UserController.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace ProjectDemo.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/")]
     [ApiController]
     public class UserController : ControllerBase
     {
